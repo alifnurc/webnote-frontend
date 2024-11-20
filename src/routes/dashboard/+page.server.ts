4ccef6f2cd61d8530e898e7a71b59c26f301245a
hitagi <alifnc@proton.me>
2024-11-20 17:39:15 +0700

    update: added listnotes on the dashboard

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,42 @@
+import { fail, redirect } from "@sveltejs/kit";
+
+export const prerender = false;
+
+export async function load({ cookies }) {
+  const data = cookies.get("jwt");
+
+  if (!data) {
+    throw redirect(303, "/signin");
+  }
+
+  const user = JSON.parse(data);
+
+  const params = new URLSearchParams({
+    page_size: "10",
+    current_page: "1",
+  });
+
+  const response = await fetch(`http://127.0.0.1:5000/listnotes?${params}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + user.access_token,
+    },
+  });
+
+  if (!response.ok) {
+    return fail(400, { error: await response.text() });
+  }
+
+  const notes = await response.json();
+  return {
+    user: user.username,
+    notes: notes.notes
+  };
+}
+
+export const actions = {
+  logout: async ({ cookies }) => {
+    cookies.delete("jwt", { path: "/" });
+    throw redirect(303, "/signin");
+  },
+};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+  let { data } = $props();
 </script>
 
-<h1 class="text-center font-bold">Welcome</h1>
+<h1 class="text-center font-bold">Welcome {data.user}</h1>
+
+{#if data.notes}
+  <ul>
+    {#each data.notes as note}
+      <li>{note.title}</li>
+    {/each}
+  </ul>
+{/if}
+
+<form action="?/logout" method="post">
+  <button type="submit">Logout</button>
+</form>
